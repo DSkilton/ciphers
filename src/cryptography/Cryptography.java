45592bd7604b920f96b7d2eee0d15fb0ffb48424
Duncan R Skilton <MC03353@mbro.ac.uk>
2019-12-18 09:21:18 +0000

    SHA256 and Caeser Cipher

---
+++ src/cryptography/Cryptography.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cryptography;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author MC03353
+ */
+
+//        https://www.geeksforgeeks.org/caesar-cipher-in-cryptography/
+//        http://practicalcryptography.com/ciphers/
+
+public class Cryptography {
+    public static Scanner sc = new Scanner (System.in);// in the real world, you would not make this public
+    public static String plainText; //this could be encapsulated (written in a separate class and declared private) to restric access
+    public static byte[] hash; 
+    
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        
+        System.out.print("Input text to be hashed: "); 
+        plainText = sc.nextLine();
+        
+        System.out.print("\nInput shift: "); 
+        int shift = sc.nextInt();
+        
+        caeserCypher.ceaserCypher(plainText, shift);
+//        Sha256.cryptoLibrary(Cryptography.plainText); 
+//        Sha256.byteToHex(Cryptography.hash);
+    }
+}
---
+++ src/cryptography/Sha256.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cryptography;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author MC03353
+ */
+public class Sha256 {
+
+    public static String cryptoLibrary(String originalInput) throws NoSuchAlgorithmException {
+        String hashedString = null;
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        Cryptography.hash = messageDigest.digest(originalInput.getBytes(StandardCharsets.UTF_8));
+        return hashedString;
+    }
+
+    public static String byteToHex(byte[] hash) {
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(255 & hash[i]);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        System.out.println(hexString);
+        return hexString.toString();
+    }   
+}
